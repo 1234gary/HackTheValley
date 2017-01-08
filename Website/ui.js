38d9a0aae76f3877275e1af2977bb84620092c4c
mikeyin97 <yinjitong@hotmail.com>
2017-01-08 00:27:11 -0500

    third round of commits

--- Website/ui.js
+++ Website/ui.js
@@ -1,11 +1,17 @@
-/* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
-var acc = document.getElementById("animal");
-var i;
-alert(acc);
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-  }
-}
+$(document).ready(function(){
+    $("#but1").click(function(){
+        $("#panel1").toggle();
+    });
+    $("#but2").click(function(){
+        $("#panel2").toggle();
+    });
+    $("#but3").click(function(){
+        $("#panel3").toggle();
+    });
+    $("#but4").click(function(){
+        $("#panel4").toggle();
+    });
+    $("#but5").click(function(){
+        $("#panel5").toggle();
+    });
+});
