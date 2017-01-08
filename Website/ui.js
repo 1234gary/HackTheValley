bbcd39b396d8d833497c84796bc3c384ac27b570
mikeyin97 <yinjitong@hotmail.com>
2017-01-08 04:47:47 -0500

    done website

--- Website/ui.js
+++ Website/ui.js
@@ -17,20 +17,47 @@ $(document).ready(function(){
         $("#panel5").toggle();
     });
     $("#chatsubmit").click(function(){
+        var value = $('#msg').val();
+        var check = 1;
+        if ((value.toLowerCase() == "hi")||(value.toLowerCase() == "hello")||(value.toLowerCase() == "hey")||(value.toLowerCase() == "hi!")||(value.toLowerCase() == "hello!")||(value.toLowerCase() == "hey!")){
+            check = 0;
+        }
         if (i == 1){
+            if (check == 0){
+            $("#message1").text(value);
             $("#message1").css("color", "black");
+            }
+            else {
+            $("#message1").text(value);
+            $("#message1").css("color", "black"); 
+            }
             
+            if (check == 0){
             setTimeout(function(){
   $("#message2").css("color", "black");
 }, 1000);
             $("#myform")[0].reset();
             i+=1;
         }
+            
+            else {
+            setTimeout(function(){
+                $("#message2").text("idk");
+  $("#message2").css("color", "black");
+}, 1000);
+            $("#myform")[0].reset();
+            i+=1;
+        } 
+            }
+            
         else{
+            var value = $('#msg').val();
+            
+            $("#message3").text(value);
             $("#message3").css("color", "black");
             setTimeout(function(){
   $("#message4").css("color", "black");
-}, 1000);
+}, 2000);
             $("#myform")[0].reset();
         }
     });
